637c3098673c156b87082f2d18ee4ae813b6775a
lcorsini <ludovico.corsini@certilogo.com>
2026-04-26 19:20:36 +0200

    fix: revert Stripe API version to match installed package
    - Use 2026-02-25.clover (from stripe@20.4.1)
    - Previous version 2024-12-18.acacia was not compatible

--- src/payments/stripe.service.ts
+++ src/payments/stripe.service.ts
@@ -15,7 +15,7 @@ export class StripeService {
     }
 
     this.stripe = new Stripe(secretKey, {
-      apiVersion: '2024-12-18.acacia',
+      apiVersion: '2026-02-25.clover',
       typescript: true,
     });
 
